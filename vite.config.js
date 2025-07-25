c6ead1bfc8d709324eeb3e68a6b270d8bde1b849
Leticia Medeiros <leticiamedeiroscosta31@Gmail.com>
2025-07-25 20:12:10 -0300

    ajuste

--- vite.config.js
+++ vite.config.js
@@ -1,11 +1,10 @@
-import { defineConfig } from 'vite';
-import react from '@vitejs/plugin-react';
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
-  resolve: {
-    alias: {
-      '@services': '/src/services',  // Aliases para importações
-    },
-  },
-});
+  base: '/cadastro-de-usuarios/',
+  build: {
+    outDir: 'dist'
+  }
+})
